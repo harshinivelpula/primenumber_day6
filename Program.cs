c9e1a267a66321c02786f65c7d06e377511b4784
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-19 22:52:01 +0530

    checkprime

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System;
+public class PrimeNumberExample
+{
+    public static void Main(string[] args)
+    {
+        int n, i, m = 0, flag = 0;
+        Console.WriteLine("enter the number to check prime: ");
+        n = int.Parse(Console.ReadLine());
+        m = n / 2;
+        for (i = 2; i <= m; i++)
+        {
+            if (n % i == 0)
+            {
+                Console.WriteLine("number is not prime.");
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+            Console.WriteLine("number is prime.");
+    }
+}
